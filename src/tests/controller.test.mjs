32686cd5fdd15565cd1a7119fa2f701dd0e53422
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-16 13:57:23 +0000

    controller unit test thru jest

--- src/tests/controller.test.mjs
+++ src/tests/controller.test.mjs
@@ -1,49 +1,63 @@
+import { jest } from '@jest/globals'
 import { regCredVerification } from '../controllers/authController'
 import { PrismaClient } from '@prisma/client'
-import { jest } from '@jest/globals'
 
-jest.mock('@prisma/client', () => {
+//so that we can change the return value of findFirst and Unique for the two tests required
+const mockFindFirst = jest.fn()
+const mockFindUnique = jest.fn()
+
+ jest.mock('@prisma/client', () => {
   return {
-    PrismaClient: jest.fn(() => ({
+    PrismaClient: jest.fn().mockImplementation(() => ({
       user: {
-        findFirst: jest.fn(),
-        findUnique: jest.fn()
+        findFirst: mockFindFirst,
+        findUnique: mockFindUnique
       },
       $disconnect: jest.fn()
     }))
   }
 })
 
+
 let prisma
 
-describe('Credential Verification', ()=>{  
+describe('Credential Verification',  ()=>{  
     //so that each new test is fresh
     beforeEach(() => {
          prisma = new PrismaClient()
-         jest.clearAllMocks()//clears data from previous test
+         mockFindFirst.mockReset()
+         mockFindUnique.mockReset()
     })
 
     afterAll(async () => {
         await prisma.$disconnect(); // Cleanup
      })
 
-    it('should return 400 if the username & email are already in use', async ()=>{
-
-           // Mock Prisma response--> these prsima functions will give out these responses when called for tests and not access the DB for real
-            prisma.user.findFirst.mockResolvedValue({
-                 username: "gaurab7"
-             })
-            prisma.user.findUnique.mockResolvedValue({
-                email: "gaurab@gmail.com"
-            })
-            const req = { body: {"r_email" : "gaurab@gmail.com", "password" : "jgjawo","r_username" : "gaurab7" } }
+    it('should return 400 if the username or email are already in use', async ()=>{
+            mockFindFirst.mockResolvedValue({id:1})//find gives an obj if it exists
+            mockFindUnique.mockResolvedValue({id:1})
+            const req = { body: {"email" : "gaurab@gmail.com", "password" : "J41&fjgjawo","username" : "gaurab7" } }
             const res = {
                 status: jest.fn().mockReturnThis(),//mockreturn makes status return res which in this context has value 400, jest.fn tracks when its called
                 json: jest.fn()
             }
-            const next = jest.fn()
             //it takes the emai and username from req, directly db is not accesed, instead whenever prisma is used/called, it uses the mock values we set above
-            await regCredVerification(req, res, next)
+            await regCredVerification(req, res)
             expect(res.status).toHaveBeenCalledWith(400)
+    })
+        
+
+    it('should return 200 if the username & email are not already in use', async ()=>{
+      //if we dont do this the mock call returns value set before .i.e. gaurab@gmail.com and gaurab7, now it returns null which means no user with the given email and username
+            mockFindFirst.mockResolvedValue(null)
+            mockFindUnique.mockResolvedValue(null)
+            //using same input values for multiple tests is not good, kept runnig into errors as mocks might retain state, internal logic of controller may still asume in the 2nd test that the vlaue is still taken
+            const req = { body: {"email" : "gaura@gmail.com", "password" : "J41&fjgjawo","username" : "gaura" } }
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            }
+            await regCredVerification(req, res)
+            expect(res.status).toHaveBeenCalledWith(200)
     })
 })
