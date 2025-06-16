49650a13cb392cd18060784ff7e2cb79afd2132c
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-16 13:45:05 +0000

    authentication controller function complete

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,30 @@
+//import bcrypt from 'bcryptjs'
+//import jwebtoken from 'jsonwebtoken'
+import { PrismaClient } from '@prisma/client'
+import { registration } from '../services/authServices.js'
 
-export function test()
-{
-console.log('validation succes')
+
+
+const prisma = new PrismaClient()
+
+export async function regCredVerification(req, res) {
+    try{
+        const { email, password, username} = req.body
+         if(await prisma.user.findUnique( {where: { email : email }}))
+             {
+                    return res.status(400).json({ error: "Email already in use!"})
+                  }
+         else if(await prisma.user.findFirst( { where: { username: username}})){
+                     return res.status(400).json({ error: "Username already in use!"})
+                  }
+         else{
+                      await registration(email, password, username)
+                      return res.status(200).json({ credentials_verified: "call services next"})
+                  }
+
+    } catch(err) {
+        console.log(err)
+        return res.status(400).json({ error: err})
+    }
+    
 }
