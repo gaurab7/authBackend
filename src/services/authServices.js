a3f7c78ae41d7650c1fb5c4496f8ac934b5daab0
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-17 03:04:45 +0000

    added login services

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -22,6 +22,28 @@ export async function registration(email, password, username) {
     }catch(err)
     {
         console.log(err)
-        return res.status(500).json({ error: "Internal Server Error"})
+        throw new Error(err.message || "Internal Server Error")
+    }
+}
+
+export async function login(user, password) {
+    try{
+        const match = await bcrypt.compare(password, user.password)//it hashes the pw user provides and compares wiht the hashed pw from DB. Returns true or false
+        if(match) { 
+                if(!process.env.JWT_SECRET_KEY) { throw new Error("JWT KEY not set in enviroment variables")}
+                const log_token =  jwt.sign({ id: user.id}, process.env.JWT_SECRET_KEY, { expiresIn: '1h' })
+                const log_tokenGen = await prisma.session.create({
+                     data: { userId: user.id, token: log_token, createdAt: new Date(Date.now()), expiresAt: new Date(Date.now() + 60 * 60 * 1000) }
+                    })
+                console.log("User Logged In")
+                return log_token
+               
+            }
+        else{
+            throw new Error(" Password Incorrect")
+        }
+    }catch(err){
+        console.log(err)
+        throw new Error(err.message || "Internal Server Error")
     }
 }
