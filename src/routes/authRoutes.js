cc843df63e9cdd6187c601a3b9f89d23c951d576
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-17 13:56:05 +0000

    added logout routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { reqValidator } from '../middleware/ authValidator.js'
 import { regCredVerification, logCredVerification } from '../controllers/authController.js'
-import { login_limiter, reg_limiter } from '../middleware/rate_limiter.js'
-
+import { login_limiter, reg_limiter, logout_limiter } from '../middleware/rate_limiter.js'
+import { tokenVerification } from '../middleware/authMiddleware.js'
+import { logout } from '../services/authServices.js'
 const router = express.Router()
 
 //registration
@@ -12,8 +13,6 @@ router.post('/register', reqValidator, reg_limiter, regCredVerification)
 router.post('/login', reqValidator, login_limiter,  logCredVerification)
 
 //logout
-router.post('/logout', (req, res)=>{
-    
-})
+router.post('/logout', logout_limiter, tokenVerification,  logout)
 
 export default router
