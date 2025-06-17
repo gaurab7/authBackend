f6ac73cd8f7e6556d4c38fb5c5b2bcc58d62f250
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-17 03:03:21 +0000

    added login routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,15 @@
 import express from 'express'
 import { reqValidator } from '../middleware/ authValidator.js'
-import { regCredVerification } from '../controllers/authController.js'
+import { regCredVerification, logCredVerification } from '../controllers/authController.js'
+import { login_limiter, reg_limiter } from '../middleware/rate_limiter.js'
 
 const router = express.Router()
 
 //registration
-router.post('/register', reqValidator, regCredVerification)
+router.post('/register', reqValidator, reg_limiter, regCredVerification)
 
 //login
-router.post('/login', (req, res)=>{
-    
-})
+router.post('/login', reqValidator, login_limiter,  logCredVerification)
 
 //logout
 router.post('/logout', (req, res)=>{
