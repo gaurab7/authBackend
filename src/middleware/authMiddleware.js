bdeef5ebafa86f2059375ad522b3550f900ae5bf
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-17 13:54:18 +0000

    added token verification

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,3 +1,31 @@
 // authentication middleware not really necessary for backend with only public api requests like register and login
 //this will only be needed when we need to alter user profile values for which we would need JWT key verification
 //.i.e unless using protected requests ( get /user, post/user) not really needed
+import { PrismaClient } from '@prisma/client'
+import jwt from 'jsonwebtoken'
+
+const prisma = new PrismaClient()
+
+export async function tokenVerification(req, res, next) {
+     const authHeader = req.headers['authorization']
+     const token = authHeader && authHeader.split(' ')[1]
+     console.log(token)
+     if(!token){ return res.status(404).json({error: "Token Not Found"})}
+     //verify takes a given token from client side, breaks it into--> header, payload, sign and then takes header+payload and applies a hash algo with the JWT Secret KEY we give as the new sign
+     //during token gen, we gave it JWT SECRET KEY as the sign and as we are doing the same now, the resulting new token should be the same as the one we got from client side
+     //this just verifies that it is a valid token, it doesnt verify if it is the token we want for protected opts
+     //for opts like logout, profile update we call services and it commpares the valid token from here and the token in DB to check if the user can acces stuff
+     jwt.verify(token, process.env.JWT_SECRET_KEY, function(err, decoded) {
+        if (err) {
+            console.log(err)
+            return res.status(401).json({msg: err.message || 'Invalid Token'})
+         }
+
+         //the payload during token gen was userid, now we can attach it to the req so that we can identify which session to delete
+         req.userid = decoded.id
+         //session table has id as prim key and token as unique so it needs one of those two in the condition to delete
+         req.token = token
+         next()
+        })
+    
+}
